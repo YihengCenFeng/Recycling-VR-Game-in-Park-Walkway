8c51b7b952391186658459ac4a249fa2b1f4f6ae
YihengCenFeng <yihengcen@gmail.com>
2021-04-11 00:44:41 -0400

    Added obstacle script

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player" || other.transform.parent.name == "TrashBinHolder")
+        {
+            FindObjectOfType<VRMovement>().Die();
+            FindObjectOfType<AudioManager>().PlaySound("Incorrect");
+        }
+    }
+}
