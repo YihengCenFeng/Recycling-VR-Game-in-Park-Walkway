4f441d53489d753044cc5d44ff99b8e6dd92203d
YihengCenFeng <yihengcen@gmail.com>
2021-04-17 18:10:12 -0400

    Added jump to VR movement

--- Assets/Scripts/VRMovement.cs
+++ Assets/Scripts/VRMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine.SceneManagement;
 public class VRMovement : MonoBehaviour
 {
     public Transform vrCamera;
+    private Vector3 direction;
     public float speed = 5f;
+    public float jumpForce = 1.0f;
+    private float gravity= -9.81f;
     public float toggleAngle = 10f;
     bool moveForward;
     bool alive = true;
@@ -22,14 +25,20 @@ public class VRMovement : MonoBehaviour
         if (!alive) return;
 
         Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+        moveForward = true;
 
         if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90f)
             moveForward = false;
-        else
-            moveForward = true;
 
-        if(moveForward)
+        if (moveForward)
             cc.SimpleMove(forward * speed);
+
+        if (vrCamera.eulerAngles.x <= 360 - toggleAngle && vrCamera.eulerAngles.x > 270f)
+            direction.y = jumpForce * -1.0f * gravity;
+        
+        direction.y += gravity * Time.deltaTime;
+        cc.Move(direction * Time.deltaTime);
+
     }
 
     private void Update()
